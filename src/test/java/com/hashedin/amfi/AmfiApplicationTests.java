f17cb2cc2e03fb4518684b547121545f0cb75be8
Krishnan <sripkrishnan@deloitte.com>
2021-02-17 10:33:57 +0530

    Scaffold from spring initializr

---
+++ src/test/java/com/hashedin/amfi/AmfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashedin.amfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
